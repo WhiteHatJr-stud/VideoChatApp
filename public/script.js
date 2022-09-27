b72ebe55308256010e88966cbc2be2c3323d0405
WhiteHatJr-stud <62693414+WhiteHatJr-stud@users.noreply.github.com>
2022-09-27 18:16:16 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,47 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {path: "/peerjs", host: "/", port: "443",});
+
+const user = prompt("Enter Your Name")
+
+$(function (){
+    $("#show_chat").click(function(){
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header-back").css("display", "block")
+    })
+    $(".header-back").click(function(){
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header-back").css("display", "none")
+    })
+    $("#send").click(function(){
+        if($('#chat_message').val().length !== 0){
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+    $("#chat_message").keydown(function(e){
+        if(e.key=="Enter" && $('#chat_message').val().length !== 0){
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
+
+peer.on("open", (id)=>{
+    socket.emit("join-room", ROOM_ID, id, user);
+})
+
+socket.on("createMessage", (message, userName)=>{
+    $(".messages").append(`
+    <div class="message">
+        <b>
+            <i class="far fa-user-circle"></i>
+            <span> ${userName === user?"me": userName}</span>
+        </b>
+        <span> ${message} </span>
+    </div>
+    `)
+})
